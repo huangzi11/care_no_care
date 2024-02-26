de0906733a888eae0eab96629b4cf2160a573818
huangzi11 <zlhuang@uw.edu>
2024-02-26 14:04:11 -0800

    edit singin and header

--- src/pages/LogOut.js
+++ src/pages/LogOut.js
@@ -5,30 +5,32 @@ import { signOut, getAuth } from "firebase/auth";
 // Takes in setShowLogOut function
 // Takes in show status
 export default function LogOutModal(props) {
-    // Closes out of modal
-    const closeOut = () => {
-        props.setShowLogOut(false);
-    }
-    // Logs user out
-    const handleLogOut = () => {
-        signOut(getAuth());
-        closeOut();
-    }
+  // Closes out of modal
+  const closeOut = () => {
+    props.setShowLogOut(false);
+  };
+  // Logs user out
+  const handleLogOut = () => {
+    signOut(getAuth());
+    closeOut();
+  };
 
-    return (
-        <Modal show={props.show} id="signOutModal" >
-            < Modal.Header >
-                <Modal.Title>Log out of sEATtle?</Modal.Title>
-            </Modal.Header >
-            <form onSubmit={handleLogOut}>
-                <Modal.Body>
-                    This will log you out of sEATtle.
-                </Modal.Body>
-                <Modal.Footer>
-                    <button type="submit" className="btn btn-default">Log Out</button>
-                    <button type="button" className="btn btn-default" onClick={closeOut}>Stay logged in</button>
-                </Modal.Footer>
-            </form>
-        </Modal >
-    )
+  return (
+    <Modal show={props.show} id="signOutModal">
+      <Modal.Header>
+        <Modal.Title>Log out of Care no Care?</Modal.Title>
+      </Modal.Header>
+      <form onSubmit={handleLogOut}>
+        <Modal.Body>Are you sure you want to log out?</Modal.Body>
+        <Modal.Footer>
+          <button type="submit" className="btn btn-default">
+            Log Out
+          </button>
+          <button type="button" className="btn btn-default" onClick={closeOut}>
+            Stay logged in
+          </button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  );
 }
